48a4c3cfffdf5d640e9e9f05c7a8f73de9ac1d92
k1hyojin <rnjsgywls96@hanmail.net>
2022-12-07 23:52:05 +0900

    home router, slide

--- src/views/MyList.vue
+++ src/views/MyList.vue
@@ -4,10 +4,23 @@
 
 <script>
 export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {
+    // const arr = [];
+    if (localStorage.length > 0) {
+      // for (let i = 0; i > localStorage.length; i++) {
+      //   // arr.push(localStorage.getItem(id))
+      //   // arr.push(JSON.parse(localStorage.getItem((localStorage.key(i)))));
 
-}
+      // }
+      console.log(JSON.parse(localStorage.getItem("store")));
+    }
+  },
+};
 </script>
 
 <style>
-
 </style>
